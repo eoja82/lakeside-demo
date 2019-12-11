af497281c20780ac586fdb7b5148da0323a89cf0
Erik Oja <erik_oja@yahoo.com>
2019-12-10 19:05:51 -0600

    create contact page and css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,39 @@
 import React from "react"
 import Layout from "../components/layout"
+import styles from "./contacts.module.css"
 
 export default () => {
   return (
     <Layout>
-      <div>Contact Coming Soon!</div>
+      <h1>Contact Lakeside Lumber & Hardware</h1>
+      <p className={styles.about}>No project is too big or to small.  <a href="tel:1-218-832-3300">Call</a>, email, or contact us on <a href="https://www.facebook.com/pg/LakesideMarcell/photos/?ref=page_internal" target="_blank" rel="noopener noreferrer">Facebook <i className="fa fa-facebook-square"></i></a>.  
+      </p>
+      <p className={styles.about}>We are here to help you with all your building and project needs!</p>
+      <div id={styles.employees}>
+        <div className={styles.employee}>
+          <a href="mailto:eric@lakesidemarcell.com">
+            <img src="/img/employees/eric.jpg" alt="employee" />
+          </a>
+          <a href="mailto:eric@lakesidemarcell.com">
+            Email Eric Elhardt
+          </a>
+        </div>
+        <div className={styles.employee}>
+          <a href="mailto:kim@lakesidemarcell.com">
+            <img src="/img/employees/kim.jpg" alt="employee" /></a>
+          <a href="mailto:kim@lakesidemarcell.com">
+            Email Kim Elhardt
+          </a>
+        </div>
+        <div className={styles.employee}>
+          <a href="mailto:matt@lakesidemarcell.com">
+            <img src="/img/employees/matt.jpg" alt="employee" />
+          </a>
+          <a href="mailto:matt@lakesidemarcell.com">
+            Email Matt Elhardt
+          </a>
+        </div>
+      </div>
     </Layout>
   )
 }

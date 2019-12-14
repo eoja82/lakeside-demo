06583479216422386ac21583662fe6a6bbd109eb
Erik Oja <erik_oja@yahoo.com>
2019-12-14 16:19:50 -0600

    remove comments

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,27 +1,68 @@
 import React from "react"
 import Layout from "../components/layout"
 import Display from "../components/products/products.js"
+//import Sidebar from "../components/products/prodSidebar.js"
 import { graphql } from "gatsby";
+//import styles from "./product.module.css"
 
 export default ({ data }) => {
   return (
     <Layout>
-      <Display product={data.dataJson} />
+      {/* <div id={styles.layout}>
+        <Sidebar /> */}
+        <Display product={data.dataJson} />
+     {/*  </div> */}
     </Layout>
   )
 }
+
+/* export const query = graphql`
+  query ($slug: String!) {
+    dataJson(slug: {eq: $slug}) {
+      description
+      slug
+      product {
+        link
+        manufacturer
+        type
+        img {
+          src {
+            childimageSharp {
+              fluid {
+                src
+              } 
+            }
+          }
+        }
+      }
+      images {
+        image {
+          src {
+            childimageSharp {
+              fluid {
+                src
+              }
+            }
+          }
+        }
+        type
+      }
+    }
+  }
+` */
+
 export const query = graphql`
   query ($slug: String!) {
     dataJson(slug: {eq: $slug}) {
       description
       images {
-        image
+        image 
         type
       }
       product {
         link
         manufacturer
-        logo
+        logo 
         type
       }
       slug

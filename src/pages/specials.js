cbcbe21d9a4c71967a2dd3bb2651136f369efaaa
Erik Oja <erik_oja@yahoo.com>
2019-12-18 16:35:59 -0600

    add facebook embed on specials page

--- src/pages/specials.js
+++ src/pages/specials.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Layout from "../components/layout"
+import styles from "./specials.module.css"
 
 export default () => {
   return (
     <Layout>
-      <div>Specials Coming Soon!</div>
+      <div id={styles.facebook}>
+        <div className="fb-page" data-href="https://www.facebook.com/pg/LakesideMarcell/photos/?ref=page_internal" data-tabs="timeline" data-width="500" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="true" data-show-facepile="true"><blockquote cite="https://www.facebook.com/pg/LakesideMarcell/photos/?ref=page_internal" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/pg/LakesideMarcell/photos/?ref=page_internal">Lakeside Lumber &amp; Hardware LLC</a></blockquote></div>
+      </div>
     </Layout>
   )
 }

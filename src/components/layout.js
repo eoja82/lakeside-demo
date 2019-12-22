b7c29befe88fcde0a89d3a0bea4261b491ab42f1
Erik Oja <erik_oja@yahoo.com>
2019-12-21 19:06:25 -0600

    move facebook div from layout.js to specials.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,7 +22,6 @@ export default ({ children }) => {
           integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
           integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <div id="fb-root"></div>
         <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v5.0"></script>
       </Helmet>
       <Navigation />

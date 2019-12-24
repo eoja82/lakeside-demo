605905f7024d038f0b05a3fbc5674cf439785d83
Erik Oja <erik_oja@yahoo.com>
2019-12-24 13:44:06 -0600

    add bg img, margin padding adjustment

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,33 +5,35 @@ import styles from "./contacts.module.css"
 export default () => {
   return (
     <Layout>
-      <h1 className={styles.about}>Contact Lakeside Lumber & Hardware</h1>
-      <p className={styles.about}>No project is too big or to small.  <a href="tel:1-218-832-3300">Call</a>, email, or contact us on <a href="https://www.facebook.com/pg/LakesideMarcell/photos/?ref=page_internal" target="_blank" rel="noopener noreferrer">Facebook <i className="fa fa-facebook-square"></i></a>.  
-      </p>
-      <p className={styles.about}>We are here to help you with all your building and project needs!</p>
-      <div id={styles.employees}>
-        <div className={styles.employee}>
-          <a href="mailto:eric@lakesidemarcell.com">
-            <img src="/img/employees/eric.jpg" alt="employee" />
-          </a>
-          <a href="mailto:eric@lakesidemarcell.com">
-            Email Eric Elhardt
-          </a>
-        </div>
-        <div className={styles.employee}>
-          <a href="mailto:kim@lakesidemarcell.com">
-            <img src="/img/employees/kim.jpg" alt="employee" /></a>
-          <a href="mailto:kim@lakesidemarcell.com">
-            Email Kim Elhardt
-          </a>
-        </div>
-        <div className={styles.employee}>
-          <a href="mailto:matt@lakesidemarcell.com">
-            <img src="/img/employees/matt.jpg" alt="employee" />
-          </a>
-          <a href="mailto:matt@lakesidemarcell.com">
-            Email Matt Elhardt
-          </a>
+      <div id={styles.container}>
+        <h1 className={styles.about}>Contact Lakeside Lumber & Hardware</h1>
+        <p className={styles.about}>No project is too big or to small.  <a className={styles.link} href="tel:1-218-832-3300"><i className="fa fa-phone"></i> Call</a>, <a className={styles.link}href="mailto:eric@lakesidemarcell.com"><i className="fa fa-envelope-square"></i> email</a>, or contact us on <a className={styles.link} href="https://www.facebook.com/pg/LakesideMarcell/photos/?ref=page_internal" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook-square"> Facebook</i></a>.  
+        </p>
+        <p className={styles.about}>We are here to help you with all your building and project needs!</p>
+        <div id={styles.employees}>
+          <div className={styles.employee}>
+            <a className={styles.link} href="mailto:eric@lakesidemarcell.com">
+              <img className={styles.employeeImg} src="/img/employees/eric.jpg" alt="employee" />
+            </a>
+            <a className={styles.link} href="mailto:eric@lakesidemarcell.com">
+              Email Eric Elhardt
+            </a>
+          </div>
+          <div className={styles.employee}>
+            <a className={styles.link} href="mailto:kim@lakesidemarcell.com">
+              <img className={styles.employeeImg} src="/img/employees/kim.jpg" alt="employee" /></a>
+            <a className={styles.link} href="mailto:kim@lakesidemarcell.com">
+              Email Kim Elhardt
+            </a>
+          </div>
+          <div className={styles.employee}>
+            <a className={styles.link} href="mailto:matt@lakesidemarcell.com">
+              <img className={styles.employeeImg} src="/img/employees/matt.jpg" alt="employee" />
+            </a>
+            <a className={styles.link} href="mailto:matt@lakesidemarcell.com">
+              Email Matt Elhardt
+            </a>
+          </div>
         </div>
       </div>
     </Layout>

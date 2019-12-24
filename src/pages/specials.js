0c726cf7fadd84c15d4fd34ce66be92e44a6562e
Erik Oja <erik_oja@yahoo.com>
2019-12-24 13:41:53 -0600

    add background image, margin padding adjustment

--- src/pages/specials.js
+++ src/pages/specials.js
@@ -6,11 +6,13 @@ class Specials extends React.Component {
   render() {
     return (
       <Layout>
-        <p>Scroll through the Facebook feed to see our specials.</p>
-        <p id={styles.p6}>
-          If you don't see the specials click <a href="https://www.facebook.com/pg/LakesideMarcell/posts/?ref=page_internal" target="_blank" rel="noopener noreferrer">here </a>to view the specials on Facebook.
-        </p>
-        <iframe id={styles.facebook} src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLakesideMarcell%2F&tabs=timeline&small_header=true&width=500&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" style={{border: "none", overflow: "hidden"}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+        <div id={styles.container}>
+          <p>Scroll through the Facebook feed to see our specials.</p>
+          <p id={styles.p6}>
+            If you don't see the specials click <a id={styles.link} href="https://www.facebook.com/pg/LakesideMarcell/posts/?ref=page_internal" target="_blank" rel="noopener noreferrer">here</a> to view the specials on Facebook.
+          </p>
+          <iframe id={styles.facebook} src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLakesideMarcell%2F&tabs=timeline&small_header=true&width=500&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" style={{border: "none", overflow: "hidden"}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+        </div>
       </Layout>
     )
   }

adb5bab7257630cff005f6ad2b793619efce6939
Erik Oja <erik_oja@yahoo.com>
2019-12-25 13:27:11 -0600

    add imgs and restyle

--- src/pages/rental.js
+++ src/pages/rental.js
@@ -5,52 +5,70 @@ import styles from "./rental.module.css"
 export default () => {
   return (
     <Layout>
-      <h4>
-        We have a selection of rental equipment availble that will enable you to complete your projects efficiently.
-      </h4>
-      <div id={styles.allEquipment}>
-        <div className={styles.equipment}>
-          <ul>
-            <li className={styles.equipHeader}>Skid Steer Attachments:</li>
-            <li>Forks</li>
-            <li>Auger - 6", 9", and 15"</li>
-            <li>Brusher</li>
-            <li>Bucket</li>
-            <li>Grabber</li>
-          </ul>
-        </div>
-        <div className={styles.equipment}>
-          <ul>
-            <li className={styles.equipHeader}>Nailers:</li>
-            <li>Flooring Nailer</li>
-            <li>Coil Roofing Nailer</li>
-            <li>Finish Nailer</li>
-            <li>Framing Nailer</li>
-            <li>Brad Nailer</li>
-          </ul>
-        </div>
-        <div className={styles.equipment}>
-          <ul>
-            <li className={styles.equipHeader}>Scaffolding:</li>
-            <li>Scaffold</li>
-            <li>Planks</li>
-            <li>Wheels</li>
-            <li>Levelers</li>
-          </ul>
-        </div>
-        <div className={styles.equipment}>
-          <ul>
-            <li className={styles.equipHeader}>Insulation Blower:</li>
-            <li>Free With Purchase of $250 Insulation</li>
-            <li>100' Hose</li>
-          </ul>
-        </div>
-        <div className={styles.equipment}>
-          <ul>
-            <li className={styles.equipHeader}>Other Tools:</li>
-            <li>Pex Tool</li>
-            <li>Hammer Drill</li>
-          </ul>
+      <div id={styles.container}>
+        <h4>
+          We have a selection of rental equipment availble that will enable you to complete your projects efficiently.
+        </h4>
+        <div id={styles.allEquipment}>
+          <div className={styles.flex}>
+            <div className={styles.listDiv}>
+              <p className={styles.equipHeader}>Skid Steer Attachments:</p>
+                <ul>
+                  <li>Forks</li>
+                  <li>Auger - 6", 9", and 15"</li>
+                  <li>Brusher</li>
+                  <li>Bucket</li>
+                  <li>Root Grapple Bucket</li>
+                </ul>
+              </div>
+              <img className={styles.productImg} src="/img/rental/skidsteer.jpg" alt="skid steer" />
+          </div>
+          <div className={styles.flex}>
+            <div className={styles.listDiv}>
+              <p className={styles.equipHeader}>Nailers:</p>
+              <ul>
+                <li>Flooring Nailer</li>
+                <li>Coil Roofing Nailer</li>
+                <li>Finish Nailer</li>
+                <li>Framing Nailer</li>
+                <li>Brad Nailer</li>
+              </ul>
+            </div>
+            <img className={styles.productImg} src="/img/rental/nailer.jpg" alt="nailer" />
+          </div>
+          <div className={styles.flex}>
+            <div className={styles.listDiv}>
+              <p className={styles.equipHeader}>Scaffolding:</p>
+              <ul>
+                <li>Scaffold</li>
+                <li>Planks</li>
+                <li>Wheels</li>
+                <li>Levelers</li>
+              </ul>
+            </div>
+            <img className={styles.productImg} src="/img/rental/scaffolding.jpg" alt="scaffolding" />
+          </div>
+          <div className={styles.flex}>
+            <div className={styles.listDiv}>
+              <p className={styles.equipHeader}>Insulation Blower:</p>
+              <ul>
+                <li>Free With Purchase of $250 Insulation</li>
+                <li>100' Hose</li>
+              </ul>
+            </div>
+            <img className={styles.productImg} src="/img/rental/insulationBlower.jpg" alt="insulation blower" />
+          </div>
+          <div className={styles.flex}>
+            <div className={styles.listDiv}>
+              <p className={styles.equipHeader}>Other Tools:</p>
+              <ul>
+                <li>Drywall Lift</li>
+                <li>Pex Tool</li>
+                <li>Hammer Drill</li>
+              </ul>
+            </div>
+            <img className={styles.productImg} src="/img/rental/hammerDrill.jpg" alt="hammer drill" />
+          </div>
         </div>
       </div>
     </Layout>

e043abaa4f8ed0906a1202dd10399eaf00fafb18
Erik Oja <erik_oja@yahoo.com>
2019-12-30 12:16:58 -0600

    edit text content

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,6 @@ export default () => {
           <title>Contact Lakeside Lumber - call, email or on Facebook</title>
         </Helmet>
       <div id={styles.container}>
-        <h1 className={styles.about}>Contact Lakeside Lumber & Hardware</h1>
         <p className={styles.about}>No project is too big or to small.  <a className={styles.link} href="tel:1-218-832-3300"><i className="fa fa-phone"></i> Call</a>, <a className={styles.link}href="mailto:eric@lakesidemarcell.com"><i className="fa fa-envelope-square"></i> email</a>, or contact us on <a className={styles.link} href="https://www.facebook.com/pg/LakesideMarcell/photos/?ref=page_internal" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook-square"> Facebook</i></a>.  
         </p>
         <p className={styles.about}>We are here to help you with all your building and project needs!</p>

bda7487f4fe6d1acfa176f3106845763a72094be
Erik Oja <erik_oja@yahoo.com>
2020-01-07 14:50:40 -0600

    fix esLint warnings

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styles from "../pages/styles/index.module.css"
 
-let slides = [{src: "img/slideShow/ccHome.jpg", alt: "finished home", isActive: true, index: 0},
-              {src: "img/slideShow/ccHome2.jpg", alt: "house with housewrap", isAvtive: false, index: 1},
-              {src: "img/slideShow/delivery1.jpg", alt: "delivery truck", isAvtive: false, index: 2},
-              {src: "img/slideShow/deck.jpg", alt: "deck", isAvtive: false, index: 3},
-              {src: "img/slideShow/garageFrame.jpg", alt: "framed garage", isAvtive: false, index: 4},
-              {src: "img/slideShow/kitchen.jpg", alt: "kitchen", isAvtive: false, index: 5},
-              {src: "img/slideShow/house1.jpg", alt: "house", isAvtive: false, index: 6},
-              {src: "img/slideShow/ccFireplace.jpg", alt: "fireplace", isActive: false, index: 7},
-              {src: "img/slideShow/metalGarage.jpg", alt: "metalGarage", isAvtive: false, index: 8}
+let slides = [{src: "img/slideShow/ccHome.jpg", alt: "finished home"},
+              {src: "img/slideShow/ccHome2.jpg", alt: "house with housewrap"},
+              {src: "img/slideShow/delivery1.jpg", alt: "delivery truck"},
+              {src: "img/slideShow/deck.jpg", alt: "deck"},
+              {src: "img/slideShow/garageFrame.jpg", alt: "framed garage"},
+              {src: "img/slideShow/kitchen.jpg", alt: "kitchen"},
+              {src: "img/slideShow/house1.jpg", alt: "house"},
+              {src: "img/slideShow/ccFireplace.jpg", alt: "fireplace"},
+              {src: "img/slideShow/metalGarage.jpg", alt: "metalGarage"}
             ];
 let slideIndex = 0;
 
@@ -20,8 +20,6 @@ class Slideshow extends React.Component {
     this.playPauseRef = React.createRef();
     this.playRef = React.createRef();
     this.pauseRef = React.createRef();
-    this.carouselRef = React.createRef();
-    //this.setSlide = this.setSlide.bind(this);
     this.play = this.play.bind(this);
     this.pause = this.pause.bind(this);
     this.hideButton = this.hideButton.bind(this);
@@ -55,63 +53,44 @@ class Slideshow extends React.Component {
       this.setState({src: slides[slideIndex].src, alt: slides[slideIndex].alt});
     }
   }
-  //user clicks slide dots future project
-  /* setSlide(e) {
-    let index = e.target.getAttribute("data-index");
-    slides[slideIndex].isActive = false;
-    slideIndex = index;
-    slides[slideIndex].isActive = true;
-    e.target.className = (styles.slideDot) + " " + (styles.activeDot);
-    this.setState({src: slides[index].src, alt: slides[index].alt});
-    console.log(e.target.getAttribute("class"))
-    e.target.className = (styles.slideDot) + " " + (styles.activeDot);
-    let index = e.target.getAttribute("data-index");
-    slideIndex = index;
-    this.setState({src: slides[index].src, alt: slides[index].alt})
-    clearInterval(this.next);
-  } */
   nextSlide() {
     if (slideIndex === slides.length - 1) {
-      slides[slideIndex].isActive = false;
-      this.setState({src: slides[slideIndex].src, alt: slides[slideIndex].alt, isActive: slides[slideIndex].isActive});
       slideIndex = 0;
-      slides[slideIndex].isActive = true;
-      this.setState({src: slides[slideIndex].src, alt: slides[slideIndex].alt, isAvtive: slides[slideIndex].isActive});
+      this.setState({src: slides[slideIndex].src, alt: slides[slideIndex].alt});
     } else {
-      slides[slideIndex].isActive = false;
-      this.setState({src: slides[slideIndex].src, alt: slides[slideIndex].alt, isActive: slides[slideIndex].isActive});
       slideIndex++;
-      slides[slideIndex].isActive = true;
-      this.setState({src: slides[slideIndex].src, alt: slides[slideIndex].alt, isAvtive: slides[slideIndex].isActive});
+      this.setState({src: slides[slideIndex].src, alt: slides[slideIndex].alt});
     }
   }
   autoSlide() {
-    this.next = setInterval(this.nextSlide, 3000);
+    this.next = setInterval(this.nextSlide, 2500);
   }
   componentDidMount() {
     this.autoSlide();
   }
   componentWillUnmount() {
     clearInterval(this.next);
-    // keep slideshow at same slide when user navigates to another page on site
-    slideIndex = slideIndex;
   }
   render() {
     return (
-       <div id={styles.slideshow} onMouseEnter={this.displayButton} onMouseLeave={this.hideButton}>
-        <img src={this.state.src} alt={this.state.alt} className={styles.introSlides} />
+       <div id={styles.slideshow} onMouseEnter={this.displayButton} onMouseLeave={this.hideButton} role="presentation">
+        <img src={this.state.src} alt={this.state.alt} id={styles.introSlides} />
         {/* mouse enter/leave, hide play/pause when hover over arrows */} 
-        <a id={styles.prev} onClick={this.prevSlide} onMouseEnter={this.hideButton} onMouseLeave={this.displayButton}><i class="fa fa-angle-left"></i></a>
-        <a id={styles.next} onClick={this.nextSlide} onMouseEnter={this.hideButton} onMouseLeave={this.displayButton}><i class="fa fa-angle-right"></i></a>
-        <div id={styles.playPause} ref={this.playPauseRef} style={{display: "none"}}>
-          <i className="fa fa-play" ref={this.playRef} style={{display: "none"}} onClick={this.play} id={styles.play}></i>
-          <i className="fa fa-pause" ref={this.pauseRef} style={{display: "block"}} onClick={this.pause} id={styles.pause}></i>
+        <button id={styles.prev} onClick={this.prevSlide} onMouseEnter={this.hideButton} onMouseLeave={this.displayButton}><i className="fa fa-angle-left"></i></button>
+        <button id={styles.next} onClick={this.nextSlide} onMouseEnter={this.hideButton} onMouseLeave={this.displayButton}><i className="fa fa-angle-right"></i></button>
+        <div id={styles.playPause} ref={this.playPauseRef} style={{display: "none"}} role="presentation">
+          <button onClick={this.play} style={{display: "none"}} id={styles.play} ref={this.playRef}>
+            <i className="fa fa-play"></i>
+          </button>
+          <button onClick={this.pause} style={{display: "block"}} id={styles.pause} ref={this.pauseRef}>
+            <i className="fa fa-pause"></i>
+          </button>
         </div>
         <div id={styles.slideDots}>
           {slides.map( (x, i) => {
             let isActive = i === slideIndex ? true : false;
             return (
-              <div className={styles.slideDot + " " + (isActive ? styles.activeDot : "")} key={`${x.alt}${i}`} data-index={i} onClick={this.setSlide}></div>
+              <div className={styles.slideDot + " " + (isActive ? styles.activeDot : "")} key={`${x.alt}${i}`}></div>
             )
           })}
         </div>
@@ -120,17 +99,3 @@ class Slideshow extends React.Component {
   }
 }
 export default Slideshow;
-
-{/* <div id={styles.slideshow}>
-  <Carousel fade={true} interval={2500} pauseOnHover={false}>
-    <Carousel.Item>
-      <img src="img/slideShow/ccHome.jpg" className={styles.introSlides} alt="finished house" onMouseEnter={this.displayButton} onMouseLeave={this.hideButton} />
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src="img/slideShow/ccHome2.jpg" className={styles.introSlides} alt="house with housewrap" onMouseEnter={this.displayButton} onMouseLeave={this.hideButton} />
-    </Carousel.Item>
-    <Carousel.Item>
-      <img src="img/slideShow/delivery1.jpg" className={styles.introSlides} alt="delivery truck" onMouseEnter={this.displayButton} onMouseLeave={this.hideButton} />
-    </Carousel.Item>
-  </Carousel>
-</div> */}
--- src/pages/specials.js
+++ src/pages/specials.js
@@ -16,7 +16,7 @@ class Specials extends React.Component {
           <p id={styles.p6}>
             If you don't see the specials click <a id={styles.link} href="https://www.facebook.com/pg/LakesideMarcell/posts/?ref=page_internal" target="_blank" rel="noopener noreferrer">here</a> to view the specials on Facebook.
           </p>
-          <iframe id={styles.facebook} src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLakesideMarcell%2F&tabs=timeline&small_header=true&width=500&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" style={{border: "none", overflow: "hidden"}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+          <iframe title="facebook" id={styles.facebook} src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLakesideMarcell%2F&tabs=timeline&small_header=true&width=500&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" style={{border: "none", overflow: "hidden"}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
         </div>
       </Layout>
     )

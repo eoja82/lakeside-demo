b3a8d4815bb90631f383091544da955144c43357
Erik Oja <erik_oja@yahoo.com>
2020-01-17 13:50:03 -0600

    move prod list to list of cards, complete restyle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Layout from "../components/layout";
 import { useStaticQuery, graphql, Link } from "gatsby";
+import Helmet from "react-helmet"
 import styles from "./styles/index.module.css";
 import Slideshow from "../components/slideshow";
+import Quote from "../components/getQuote";
 
 let List = () => {
   const data = useStaticQuery(graphql`
@@ -10,24 +12,30 @@ let List = () => {
     allDataJson {
       nodes {
         slug
+        images {
+          image
+          type
+        }
       }
     }
   }
   `)
   let list = [];
-  data.allDataJson.nodes.forEach( ({slug}, i) => {
+  data.allDataJson.nodes.forEach( ({slug, images}, i) => {
     let split = slug.split("/")
     let listItem = split[2].replace(/-/g, " ");
-    list.push({path: slug, key: i, name: listItem})
+    list.push({path: slug, key: i, name: listItem, src: images[0].image, alt: images[0].type})
   });
   return (
     <div id={styles.list}>
       {list.map( x => (
         <Link to={x.path} key={x.key} className={styles.listItem}>
-          {x.name}
+          <div>
+            <img className={styles.listImg} src={x.src} alt={x.alt} />
+            <p className={styles.listP}>{x.name} <i className="fa fa-angle-right"></i></p>
+          </div>
         </Link>
       ))}
-      <Link className={styles.listItem + " " + styles.noLink}>And More!</Link>
     </div>
   )
 }
@@ -37,47 +45,38 @@ class Index extends React.Component {
     return (
       <Layout>
       <div id={styles.intro}> 
-        <div id={styles.about}>
-        <h3 id={styles.h3}>Lakeside Lumber is your source for:</h3>
-        <List />
-        </div>
         <Slideshow />
-      
+        <div id={styles.about}>
+          <h3 className={styles.h3}>Family owned and operated for over 30 years!</h3>
+        </div>
       </div>
-      <div id={styles.moreInfo}>
-        <div className={styles.flex + " " + styles.reverse}>
-          <div>
-            <img className={styles.moreInfoImg} src="/img/warehouseLogo.jpg" alt="store front" />
+      <div className={styles.moreInfo}>
+        <div className={`${styles.moreInfoIntro} ${styles.reverse}`}>
+          <h2 className={styles.moreInfoIntroH2}>
+            We are proud to serve contractors and homeowners as your local source for quality building materials.
+          </h2>
+          <img className={styles.moreInfoImg} src="/img/warehouseLogo.jpg" alt="store front" />
+        </div>
+      </div>
+      <div className={styles.containerProd}>
+        <div className={styles.productDiv}>
+          <h3 className={styles.h3}>
+            PRODUCTS
+          </h3>
+          <div className={styles.products}>
+            <List />
           </div>
-          <div>
-            <p>Family owned and operated, Lakeside Lumber offers everything a builder or homeowner will need.</p>
-            <p>​With a knowledgeable and courteous staff, we are always happy to help you with your projects.</p>
+          <div className={styles.quote}>
+            <Quote />
           </div>
         </div>
-        <div className={styles.flex + " " + styles.wrap}>
-          <div>
-            <p>We have a wide variety of treated, SPF, and cedar lumber.</p>
-          </div>
-          <div>
-            <img className={styles.moreInfoImg} src="/img/wharehouseInside.jpg" alt="inside wharehouse" />
-          </div>
-        </div>  
-        <div className={styles.flex + " " + styles.reverse}>
-          <div>
-            <img className={styles.moreInfoImg} src="/img/truckShingles.jpg" alt="delivery truck" />
-          </div>
-          <div>
-            <p>We have supplies for a new roof or to repair an existing one - from steel to tar-paper.</p>
-          </div>
-        </div>
-        <div className={styles.flex + " " + styles.wrap}>
-          <div>
-            <p>WE DELIVER!</p>
-            <p>Proudly serving the Edge of the Wilderness area - Marcell, Bigfork, Effie, as well as Deer River, Balsom, Talmoon, and Grand Rapids areas.</p>
-          </div>
-          <div>
-            <img className={styles.moreInfoImg} src="/img/loadedTruck.jpg" alt="delivery truck" />
-          </div>
+      </div>
+      <div className={styles.moreInfo}>
+        <div className={styles.moreInfoIntro}>
+          <img className={styles.moreInfoImg} src="/img/loadedTruck.jpg" alt="delivery truck" />
+          <h2 className={styles.moreInfoIntroH2}>
+            WE DELIVER! - Serving the Edge of the Wilderness area - Marcell, Bigfork, Effie, as well as Deer River, Balsom, Talmoon, and Grand Rapids areas.
+          </h2>
         </div>
       </div>
     </Layout>

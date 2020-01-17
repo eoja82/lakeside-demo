bef3ac8fd79664ca492e9cfa551a9e77499d75db
Erik Oja <erik_oja@yahoo.com>
2020-01-17 15:54:53 -0600

    add fonts

--- src/pages/specials.js
+++ src/pages/specials.js
@@ -12,11 +12,11 @@ class Specials extends React.Component {
           <meta name="description" content="Current specials at Lakeside Lumber" />
         </Helmet>
         <div id={styles.container}>
-          <p>Scroll through the Facebook feed to see our specials.</p>
-          <p id={styles.p6}>
+          <h4 className={styles.h4}>Scroll through the Facebook feed to see our specials.</h4>
+          <p id={styles.p}>
             If you don't see the specials click <a id={styles.link} href="https://www.facebook.com/pg/LakesideMarcell/posts/?ref=page_internal" target="_blank" rel="noopener noreferrer">here</a> to view the specials on Facebook.
           </p>
-          <iframe title="facebook" id={styles.facebook} src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLakesideMarcell%2F&tabs=timeline&small_header=true&width=500&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" style={{border: "none", overflow: "hidden"}} scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+          <iframe title="facebook" id={styles.facebook} src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLakesideMarcell%2F&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" style={{border: "none", overflow: "scroll", width: "500", height: "500"}} scrolling="yes" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
         </div>
       </Layout>
     )

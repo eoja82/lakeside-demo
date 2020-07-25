9099a9aac47daeb1c38aba19a1aa1ab82e0e5f3b
Erik Oja <erik_oja@yahoo.com>
2020-07-25 17:12:00 -0500

    fix warnings in iframe

--- src/pages/specials.js
+++ src/pages/specials.js
@@ -16,7 +16,7 @@ class Specials extends React.Component {
           <p id={styles.p}>
             If you don't see the specials click <a id={styles.link} href="https://www.facebook.com/pg/LakesideMarcell/posts/?ref=page_internal" target="_blank" rel="noopener noreferrer">here</a> to view the specials on Facebook.
           </p>
-          <iframe title="facebook" id={styles.facebook} src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLakesideMarcell%2F&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" style={{border: "none", overflow: "scroll", width: "500", height: "500"}} scrolling="yes" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+          <iframe title="facebook" id={styles.facebook} src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FLakesideMarcell%2F&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" style={{border: "none", overflow: "scroll", width: "500", height: "500"}} scrolling="yes" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
         </div>
       </Layout>
     )

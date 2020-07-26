8b62ed51bd8fce74958b95f4745d52d0991f97b4
Erik Oja <erik_oja@yahoo.com>
2020-07-26 15:05:00 -0500

    remove unused plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,20 +7,12 @@
 module.exports = {
   pathPrefix: "/lakeside-demo",
   plugins: [
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `./src/components/products/data/`
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `./static/img/products/`
       }
     },
   ]

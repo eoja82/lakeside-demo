8d54a6d8376a79125acdaf55c26100327588c372
Erik Oja <erik_oja@yahoo.com>
2020-07-26 19:06:10 -0500

    use withPrefix for gh pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import Navigation from "./nav"
 import Footer from "./footer"
 import { Helmet } from "react-helmet"
+import { withPrefix } from "gatsby"
 
 export default ({ children }) => {
   return (
@@ -14,7 +15,7 @@ export default ({ children }) => {
         <title>Lakeside Lumber & Hardware, your local building material supplier!</title>
         <meta name="description" content="Proudly serving the Edge of the Wilderness area - Marcell, Bigfork, Effie, as well as Deer River, Balsom, Talmoon, and Grand Rapids areas." />
         <meta name="keywords" content="lumber, roofing, windows, doors, treated, spf, paint, stain, marcell, bigfork, effie, grand rapids" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" type="image/png" href={withPrefix("/favicon.png")} />
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous" />

8d4c4652e8db46f4d1bbc7c2c27c9e6f7a5025c3
Erik Oja <erik_oja@yahoo.com>
2020-11-30 15:30:49 -0600

    delete comment

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,5 @@
 const path = require(`path`)
-/* exports.onCreateNode = ({ node, getNode, actions }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === `DataJson`) {
-    node.images.map( (x, i) => {
-      createNodeField({
-        node,
-        name: `pageImage${i}`,
-        value: x.image
-      })
-    })
 
-  }
-} */
 exports.createPages = async ({ graphql, actions}) => {
   const { createPage } = actions
   const result = await graphql(`

240c9fc829e298089c2af46741f6312e539c1f4c
Erik Oja <erik_oja@yahoo.com>
2021-10-13 16:10:08 -0500

    change quote btn from W3CSS to Bootstrap

--- src/components/getQuote.js
+++ src/components/getQuote.js
@@ -1,52 +1,34 @@
 import React from "react";
-import Helmet from "react-helmet";
 import styles from "./styles/getQuote.module.css";
 
-class Quote extends React.Component {
-  constructor(props) {
-    super(props);
-    this.openModal = this.openModal.bind(this);
-    this.closeModal = this.closeModal.bind(this);
-  }
-  openModal() {
-    document.getElementById("modal").style.display = "block";
-  }
-  closeModal(e) {
-    let modal = document.getElementById("modal");
-    let xClose = document.getElementById("xClose");
-    if (e.target === modal || e.target === xClose) {
-      modal.style.display = "none";
-    }
-  }
-  componentDidMount() {
-    window.addEventListener("click", this.closeModal);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("click", this.closeModal);
-  }
-  render() {
-    return (
-      <div>
-        <Helmet>
-          <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" /> 
-        </Helmet>
-        {/* open modal */}
-        <button id={styles.button} onClick={this.openModal}>Get A Quote</button>
-        {/* the modal */}
-        <div id="modal" onClick={this.closeModal} className="w3-modal w3-animate-zoom" role="presentation">
-          <div className={"w3-modal-content w3-card-4 " + styles.modalContent}>
-            <header className="w3-container w3-blue">
-              <span id="xClose" onClick={this.closeModal} className="w3-button w3-display-topright" role="presentation">&times;</span>
-              <h2 id={styles.h2}>Please call or email us for a quote:</h2>
-            </header>
-            <div id={styles.contactInfo}className="w3-container">
-              <p><a href="tel:1-555-555-5555"><i className="fa fa-phone"></i> 1-555-555-5555</a></p>
-              <p><a href="mailto:john@fakeemail.com"><i className="fa fa-envelope-square"></i> john@fakeemail.com</a></p>
+function Quote() {
+  return (
+    <div>
+      {/* Button triggers modal */}
+      <button type="button" className="btn btn-primary" id={styles.button} data-toggle="modal" data-target="#quoteModal">Get A Quote</button>
+
+      {/* modal */}
+      <div className="modal fade" id="quoteModal" tabIndex="-1" role="dialog" aria-labelledby="modalTitle" aria-hidden="true">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content" id={styles.modalContent}>
+            <div className="modal-header" id={styles.modalHeader}>
+              <h4 className="modal-title" id="modalTitle">Please call or email us for a quote:</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span id={styles.close} aria-hidden="true">&times;</span>
+              </button>
             </div>
+            <div class="modal-body">
+              <p><a href="tel:1-555-555-5555" style={{color: "#3e3e3e", fontSize: "larger"}}><i className="fa fa-phone"></i> 1-555-555-5555</a></p>
+              <p><a href="mailto:john@fakeemail.com" style={{color: "#3e3e3e", fontSize: "larger"}}><i className="fa fa-envelope-square"></i> john@fakeemail.com</a></p>
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+          </div>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
 export default Quote;

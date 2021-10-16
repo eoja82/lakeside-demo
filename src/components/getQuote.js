8d0deb537283bcba0c96346e6cdf309ae68bc768
Erik Oja <erik_oja@yahoo.com>
2021-10-16 16:12:15 -0500

    refactor quote btn for bootstrap 5.1

--- src/components/getQuote.js
+++ src/components/getQuote.js
@@ -5,7 +5,7 @@ function Quote() {
   return (
     <div>
       {/* Button triggers modal */}
-      <button type="button" className="btn btn-primary" id={styles.button} data-toggle="modal" data-target="#quoteModal">Get A Quote</button>
+      <button type="button" className="btn btn-primary" id={styles.button} data-bs-toggle="modal" data-bs-target="#quoteModal">Get A Quote</button>
 
       {/* modal */}
       <div className="modal fade" id="quoteModal" tabIndex="-1" role="dialog" aria-labelledby="modalTitle" aria-hidden="true">
@@ -13,16 +13,16 @@ function Quote() {
           <div className="modal-content" id={styles.modalContent}>
             <div className="modal-header" id={styles.modalHeader}>
               <h4 className="modal-title" id="modalTitle">Please call or email us for a quote:</h4>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span id={styles.close} aria-hidden="true">&times;</span>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                <span id={styles.close} aria-hidden="true"></span>
               </button>
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
               <p><a href="tel:1-555-555-5555" style={{color: "#3e3e3e", fontSize: "larger"}}><i className="fa fa-phone"></i> 1-555-555-5555</a></p>
               <p><a href="mailto:john@fakeemail.com" style={{color: "#3e3e3e", fontSize: "larger"}}><i className="fa fa-envelope-square"></i> john@fakeemail.com</a></p>
             </div>
-            <div class="modal-footer">
-            <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Close</button>
+            <div className="modal-footer">
+            <button type="button" className="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
           </div>
           </div>
         </div>

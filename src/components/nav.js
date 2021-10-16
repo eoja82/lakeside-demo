3ba6cc9b34ea725f703361313ab9a6ca42bc99c5
Erik Oja <erik_oja@yahoo.com>
2021-10-16 16:05:29 -0500

    refactor nav for bootstrap 5.1

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,88 +3,61 @@ import { StaticQuery, graphql, Link } from "gatsby";
 import styles from "./styles/nav.module.css";
 import { withPrefix } from "gatsby";
 
-class Nav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.scrollFunc = this.scrollFunc.bind(this); 
+
+function Nav() {
+  const activeStyles = {
+    color: "white"
   }
-  scrollFunc() {
-    let logo = document.getElementById("logo");
-    let links = document.querySelectorAll(".nav-link");
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      logo.style.width = "87.5px";
-      logo.style.height = "50px";
-      links.forEach( x => x.style.fontSize = "1.25rem");
-    } else {
-      logo.style.width = "140px";
-      logo.style.height = "80px";
-      links.forEach( x => x.style.fontSize = "1.5rem");
-    }
-  }
-  componentDidMount() {
-    window.addEventListener("scroll", this.scrollFunc);
-  }
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.scrollFunc);
-  }
-  render() {
-      // for active Link
-      const activeStyles = {
-        color: "white"
-      };
-    return (
-      <StaticQuery id={styles.container}
+  return (
+    <StaticQuery id={styles.container}
       query={graphql`
-      query SlugQuery {
-        allDataJson {
-          nodes {
-            slug
+        query SlugQuery {
+          allDataJson {
+            nodes {
+              slug
+            }
           }
         }
-      }
       `}
-      render={ data => (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-        <Link className={"navbar-brand " + styles.logo} to="/"><img id="logo" className={styles.logo} src={withPrefix("/img/logo.png")} alt="logo" width="140px" height="80px" /></Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarToggler">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className="nav-item dropdown">
-              <button className={"nav-link dropdown-toggle " + styles.navItem} id={styles.dropButton} data-toggle="dropdown" href="#" style={{fontSize: "1.4rem"}} aria-haspopup="true" aria-expanded="false">Products</button>
-              <div className="dropdown-menu">
-                {data.allDataJson.nodes.map( ({slug}, i) => {
-                  let split = slug.split("/")
-                  let listItem = split[2].replace(/-/g, " ");
-                  return (
-                    <Link to={slug} key={i} className="dropdown-item" id={styles.dropdown}>
-                      {listItem}
-                    </Link>
-                  )
-                })}
-              </div>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + styles.navItem} to="/rental/" style={{fontSize: "1.4rem"}} activeStyle={activeStyles}>Rental</Link>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + styles.navItem} to="/specials/" style={{fontSize: "1.4rem"}} activeStyle={activeStyles}>Specials</Link>
-            </li>
-            <li className="nav-item">
-              <Link className={"nav-link " + styles.navItem} to="/contact/" style={{fontSize: "1.4rem"}} activeStyle={activeStyles}>Contact</Link>
-            </li>
-          </ul>
-          {/* <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" />
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form> */}
-        </div>
-      </nav>
+      render={data => (
+        <nav className="navbar navbar-expand-sm fixed-top navbar-dark bg-dark text-white" style={{"--bs-bg-opacity": ".9"}}>
+          <div className="container-fluid">
+            <Link className={"navbar-brand " + styles.logo} to="/"><img id="logo" className={styles.logo} src={withPrefix("/img/logo.png")} alt="logo" width="87.5px" height="50px" /></Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarToggler">
+              <ul className="navbar-nav">
+                <li className="nav-item dropdown">
+                  <a className={"nav-link dropdown-toggle " + styles.navLink} href="#" id="navDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{fontSize: "1.4rem"}}>Products</a>
+                  <ul className="dropdown-menu" aria-labelledby="navDropdown" id={styles.dropdown}>
+                    {data.allDataJson.nodes.map(({slug}, i) => {
+                      let split = slug.split("/")
+                      let listItem = split[2].replace(/-/g, " ")
+                      return (
+                        <li>
+                          <Link to={slug} key={i} className="dropdown-item">{listItem}</Link>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <Link className={"nav-link " + styles.navLink} to="/rental/" style={{fontSize: "1.4rem"}} activeStyle={activeStyles}>Rental</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={"nav-link " + styles.navLink} to="/specials/" style={{fontSize: "1.4rem"}} activeStyle={activeStyles}>Specials</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={"nav-link " + styles.navLink} to="/contact/" style={{fontSize: "1.4rem"}} activeStyle={activeStyles}>Contact</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       )}
-      />
-    )
-  }
+    />
+  )
 }
 
 export default Nav;

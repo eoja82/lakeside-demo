72f2eca0c90be8b344f3699d6d2d97e5256a10e9
Erik Oja <erik_oja@yahoo.com>
2021-10-20 17:56:13 -0500

    comment out carousel captions

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -15,24 +15,24 @@ function Slideshow() {
         <div className="carousel-inner" id={styles.carousel}>
           <div className="carousel-item active" data-bs-interval="4000">
             <div style={{backgroundImage: `url(${withPrefix("/img/slideShow/storeFront.jpeg")})`}} className={"d-block w-100 " + styles.bgimg}></div>
-            <div className={styles.caption1}>
+            {/* <div className={styles.caption1}>
               <h1 className={styles.captionh1}>FAMILY OWNED AND OPERATED</h1>
-            </div>
+            </div> */}
           </div>
           <div className="carousel-item" data-bs-interval="4000">
             <div style={{backgroundImage: `url(${withPrefix("/img/slideShow/boomTruck1.jpg")})`}} className={"d-block w-100 " + styles.bgimg}></div>
-            <div className={styles.caption2}>
+            {/* <div className={styles.caption2}>
               <div>
                 <h1 className={styles.captionh1}>WE DELIVER</h1>
                 <p className={styles.captionp}>BOOM TRUCK AVAILABLE</p>
               </div>
-            </div>
+            </div> */}
           </div>
           <div className="carousel-item" data-bs-interval="4000" >
             <div style={{backgroundImage: `url(${withPrefix("/img/slideShow/house.jpeg")})`}} className={"d-block w-100 " + styles.bgimg}></div>
-            <div className={styles.caption3}>
+            {/* <div className={styles.caption3}>
               <h1 className={styles.captionh1}>NO JOB TOO BIG OR TOO SMALL</h1>
-            </div>
+            </div> */}
           </div>
         </div>
         <button className="carousel-control-prev" data-bs-target="#carouselCaptions" type="button" data-bs-slide="prev">

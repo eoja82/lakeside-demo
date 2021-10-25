adab0ccbe5968b772274cbcd15125a5d9816668c
Erik Oja <erik_oja@yahoo.com>
2021-10-25 14:12:29 -0500

    remove W3.CSS link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,7 +16,6 @@ class Contact extends React.Component {
       <Layout>
       <Helmet>
           <title>Contact Lakeside Lumber - call, email or on Facebook</title>
-          <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css" /> 
         </Helmet>
       <div id={styles.container} style={{backgroundImage: `url(${withPrefix("/img/graySiding.jpg")})`}}>
         <p className={styles.about}>No project is too big or to small.  <a className={styles.textLink} href="tel:1-218-832-3300">  Call</a>, <a className={styles.textLink}href="mailto:eric@lakesidemarcell.com"> email</a>, or contact us on <a className={styles.textLink} href="https://www.facebook.com/pg/LakesideMarcell/photos/?ref=page_internal" target="_blank" rel="noopener noreferrer"> Facebook</a>.<br />  We are here to help you with all your building and project needs!</p>

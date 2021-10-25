3ac9516069166e0b3969e3191f200b676bfe274b
Erik Oja <erik_oja@yahoo.com>
2021-10-25 14:18:20 -0500

    refactor nav using React-Bootstrap

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { StaticQuery, graphql, Link } from "gatsby";
-import styles from "./styles/nav.module.css";
-import { withPrefix } from "gatsby";
+import React from "react"
+import { Link, StaticQuery, graphql } from "gatsby"
+import styles from "./styles/nav.module.css"
+import { withPrefix } from "gatsby"
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import Navbar from "react-bootstrap/Navbar"
+import NavDropdown from "react-bootstrap/NavDropdown"
 
 
-function Nav() {
+function Navigation() {
   const activeStyles = {
-    color: "white"
+    color: "rgb(255, 255, 255, .75)"
   }
   return (
     <StaticQuery id={styles.container}
@@ -20,44 +24,47 @@ function Nav() {
         }
       `}
       render={data => (
-        <nav className="navbar navbar-expand-sm fixed-top navbar-dark bg-dark text-white" style={{"--bs-bg-opacity": ".9"}}>
-          <div className="container-fluid">
-            <Link className={"navbar-brand " + styles.logo} to="/"><img id="logo" className={styles.logo} src={withPrefix("/img/logo.png")} alt="logo" width="87.5px" height="50px" /></Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarToggler">
-              <ul className="navbar-nav">
-                <li className="nav-item dropdown">
-                  <a className={"nav-link dropdown-toggle " + styles.navLink} href="#" id="navDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{fontSize: "1.4rem"}}>Products</a>
-                  <ul className="dropdown-menu" aria-labelledby="navDropdown" id={styles.dropdown}>
-                    {data.allDataJson.nodes.map(({slug}, i) => {
+        <Navbar expand="sm" variant="dark" bg="dark" fixed="top" style={{"--bs-bg-opacity": ".9"}}>
+          <Container fluid>
+            <Navbar.Brand href="/">
+              <img 
+                src={withPrefix("/img/logo.png")} 
+                alt="logo" 
+                width="87.5px" height="50px">
+              </img>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-nav" />
+            <Navbar.Collapse id="navbar-nav">
+              <Nav className="me-auto">
+                <NavDropdown 
+                  title="Products"
+                  id="nav-dropdown" 
+                  className={styles.navLink}
+                >
+                  {data.allDataJson.nodes.map(({slug}, i) => {
                       let split = slug.split("/")
                       let listItem = split[2].replace(/-/g, " ")
                       return (
-                        <li>
-                          <Link to={slug} key={i} className="dropdown-item">{listItem}</Link>
-                        </li>
+                        <NavDropdown.Item href={slug} key={i} className={styles.dropdownItem}>{listItem}</NavDropdown.Item>
                       )
                     })}
-                  </ul>
-                </li>
-                <li className="nav-item">
-                  <Link className={"nav-link " + styles.navLink} to="/rental/" style={{fontSize: "1.4rem"}} activeStyle={activeStyles}>Rental</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className={"nav-link " + styles.navLink} to="/specials/" style={{fontSize: "1.4rem"}} activeStyle={activeStyles}>Specials</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className={"nav-link " + styles.navLink} to="/contact/" style={{fontSize: "1.4rem"}} activeStyle={activeStyles}>Contact</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
+                </NavDropdown>
+                <Nav.Link>
+                  <Link to="/rental" activeStyle={activeStyles} className={styles.navLink}>Rental</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/specials" activeStyle={activeStyles} className={styles.navLink}>Specials</Link>
+                </Nav.Link>
+                <Nav.Link>
+                  <Link to="/contact" activeStyle={activeStyles} className={styles.navLink}>Contact</Link>
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       )}
     />
   )
 }
 
-export default Nav;
+export default Navigation;

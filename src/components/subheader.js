2969ea57dd89f8caf0b8e1f2dcc64d34e39fbad2
Erik Oja <erik_oja@yahoo.com>
2021-11-09 16:44:30 -0600

    add prop for style

--- src/components/subheader.js
+++ src/components/subheader.js
@@ -6,7 +6,7 @@ import Container from "react-bootstrap/Container"
 export default function Subheader(props) {
   return (
     <Container>
-      <h3 className={styles.h3 + ` ${props.lightText ? styles.lightText : styles.darkText}`}>{props.text}</h3>
+      <h3 className={styles.h3 + ` ${props.lightText ? styles.lightText : styles.darkText}`} style={props.style}>{props.text}</h3>
       <div className={styles.headerUnderline}></div>
     </Container>
   )
@@ -14,5 +14,6 @@ export default function Subheader(props) {
 
 Subheader.propTypes = {
   text: PropTypes.string,
-  lightText: PropTypes.bool
+  lightText: PropTypes.bool,
+  style: PropTypes.object
 }

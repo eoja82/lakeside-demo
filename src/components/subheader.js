cc2edcd6446f64c4ddd7fbbf39c4775c96374a47
Erik Oja <erik_oja@yahoo.com>
2021-11-09 15:05:13 -0600

    add prop-types

--- src/components/subheader.js
+++ src/components/subheader.js
@@ -1,12 +1,17 @@
 import React from "react"
+import PropTypes from "prop-types"
 import styles from "./styles/subheader.module.css"
 import Container from "react-bootstrap/Container"
 
-export default (props) => {
+export default function Subheader(props) {
   return (
     <Container>
       <h3 className={styles.h3}>{props.text}</h3>
       <div className={styles.headerUnderline}></div>
     </Container>
   )
+}
+
+Subheader.propTypes = {
+  text: PropTypes.string
 }

95238b5da712bea6ba397dec6bafbec36f0bfc4d
Erik Oja <erik_oja@yahoo.com>
2021-11-15 13:48:41 -0600

    add PropTypes

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,19 @@
 import React from "react"
+import PropTypes from "prop-types"
 import styles from "./styles/header.module.css"
 import { withPrefix } from "gatsby"
 import Container from "react-bootstrap/Container"
 
-export default (props) => {
+function Header(props) {
   return (
     <Container as="header" fluid id={styles.bgImg} style={{backgroundImage: `url(${withPrefix("/img/storePano.jpg")})`}}>
       <h1 className="bg-dark" id={styles.headerText} style={{"--bs-bg-opacity": ".5"}}>{props.text}</h1>
     </Container>
   )
 }
+
+Header.prototypes = {
+  text: PropTypes.string.isRequired
+}
+
+export default Header

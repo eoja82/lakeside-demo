5f674c32502d849c289ef3dd1fe0765b97704f12
Erik Oja <erik_oja@yahoo.com>
2022-12-29 12:16:51 -0600

    fix formatting

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -22,36 +22,36 @@ class Contact extends React.Component {
   render() {
     return (
       <Layout>
-      <Helmet>
-        <title>Contact Lakeside Lumber - call, email or on Facebook</title>
-      </Helmet>
-      <Header text="CONTACT US" />
-      <Subheader text="OUR TEAM" />
-      <p className={styles.about}>No project is too big or to small.<br />We are here to help you with all your building and project needs!</p>
-      <Container style={{marginBottom: "2rem"}}>
-        <Row xs={1} sm={1} md={2} lg={3}>
-          {employees.map( x => (
-            <Col key={x.key} style={{padding: "12px"}}>
-              <Card className="text-center" style={{border: "none"}}>
-                <Image src={withPrefix(x.src)} alt={x.name} roundedCircle fluid />
-                <Card.Body>
-                  <Card.Title as="h5" className={styles.cardTitle}>{x.name}</Card.Title>
-                  <Card.Subtitle className={styles.cardSubtitle}>{x.position}</Card.Subtitle>
-                  <div className={styles.links}>
-                    <Button variant="outline-dark" className={styles.cardButton}>
-                      <Card.Link href={`tel:${x.phone}`} aria-label="phone number" className={styles.cardLink}>Call</Card.Link>
-                    </Button>
-                    <Button variant="outline-dark" className={styles.cardButton}>
-                      <Card.Link href={`mailto:${x.email}`} aria-label="email" className={styles.cardLink}>Email</Card.Link>
-                    </Button>
-                   </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </Container>
-    </Layout>
+        <Helmet>
+          <title>Contact Lakeside Lumber - call, email or on Facebook</title>
+        </Helmet>
+        <Header text="CONTACT US" />
+        <Subheader text="OUR TEAM" />
+        <p className={styles.about}>No project is too big or to small.<br />We are here to help you with all your building and project needs!</p>
+        <Container style={{ marginBottom: "2rem" }}>
+          <Row xs={1} sm={1} md={2} lg={3}>
+            {employees.map(x => (
+              <Col key={x.key} style={{ padding: "12px" }}>
+                <Card className="text-center" style={{ border: "none" }}>
+                  <Image src={withPrefix(x.src)} alt={x.name} roundedCircle fluid />
+                  <Card.Body>
+                    <Card.Title as="h5" className={styles.cardTitle}>{x.name}</Card.Title>
+                    <Card.Subtitle className={styles.cardSubtitle}>{x.position}</Card.Subtitle>
+                    <div className={styles.links}>
+                      <Button variant="outline-dark" className={styles.cardButton}>
+                        <Card.Link href={`tel:${x.phone}`} aria-label="phone number" className={styles.cardLink}>Call</Card.Link>
+                      </Button>
+                      <Button variant="outline-dark" className={styles.cardButton}>
+                        <Card.Link href={`mailto:${x.email}`} aria-label="email" className={styles.cardLink}>Email</Card.Link>
+                      </Button>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </Layout>
     )
   }
 }

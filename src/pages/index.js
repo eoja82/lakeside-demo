d95be75d576f188652ded5df3dcbc9a8aef403b2
Erik Oja <erik_oja@yahoo.com>
2022-12-29 12:16:37 -0600

    fix formatting

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,8 @@ import Row from "react-bootstrap/Row"
 
 
 
-  const List = () => {
-    const data = useStaticQuery(graphql`
+const List = () => {
+  const data = useStaticQuery(graphql`
     query SlugToQuery {
       allDataJson {
         nodes {
@@ -28,24 +28,24 @@ import Row from "react-bootstrap/Row"
         }
       }
     }
-    `)
-    let list = []
-    data.allDataJson.nodes.forEach( ({slug, products}, i) => {
-      let split = slug.split("/")
-      let listItem = split[2].replace(/-/g, " ")
-      list.push({path: slug, key: i, name: listItem, src: products[0].image.src, alt: products[0].image.alt})
-    })
+  `)
+  let list = []
+  data.allDataJson.nodes.forEach(({ slug, products }, i) => {
+    let split = slug.split("/")
+    let listItem = split[2].replace(/-/g, " ")
+    list.push({ path: slug, key: i, name: listItem, src: products[0].image.src, alt: products[0].image.alt })
+  })
   return (
     <Container id={styles.listContainer}>
       <Row xs={1} sm={1} md={2} lg={3}>
-        {list.map( x => (
-          <Col style={{padding: "12px"}} key={x.key}>
-            <Card className="text-center" style={{border: "none"}}>
+        {list.map(x => (
+          <Col style={{ padding: "12px" }} key={x.key}>
+            <Card className="text-center" style={{ border: "none" }}>
               <Link to={x.path}>
-                <Card.Img varient="top" src={withPrefix(x.src)} style={{borderBottomLeftRadius: "0", borderBottomRightRadius: "0"}} alt={x.alt} />
+                <Card.Img varient="top" src={withPrefix(x.src)} style={{ borderBottomLeftRadius: "0", borderBottomRightRadius: "0" }} alt={x.alt} />
               </Link>
               <Link to={x.path} className={styles.listP}>
-                <Card.Body className="bg-dark" style={{borderBottomLeftRadius: "calc(.25rem - 1px)", borderBottomRightRadius: "calc(.25rem - 1px)"}}>
+                <Card.Body className="bg-dark" style={{ borderBottomLeftRadius: "calc(.25rem - 1px)", borderBottomRightRadius: "calc(.25rem - 1px)" }}>
                   {x.name} <i className={`fa fa-angle-right ${styles.rightArrow}`}></i>
                 </Card.Body>
               </Link>
@@ -66,7 +66,7 @@ class Index extends React.Component {
   closeDisclaimer() {
     this.disclaimer.current.style.display = "none"
     clearTimeout(this.timer)
-  } 
+  }
   componentDidMount() {
     this.timer = setTimeout(this.closeDisclaimer, 15000)
   }
@@ -76,51 +76,51 @@ class Index extends React.Component {
   render() {
     return (
       <Layout>
-      <div id={styles.intro}> 
-        <Slideshow />
-      </div>
-      <Container fluid>
-        <Row className="bg-dark">
-          <Col md={12} lg={6} className={styles.col}>
-            <div>
-              <Subheader text="QUALITY MATERIALS" lightText style={{marginTop: 0}} />
-              <p className={styles.moreInfoP}>
-                We are proud to serve contractors and homeowners as your local source for quality building materials.
-              </p>
-            </div>
-          </Col>
-          <Col md={12} lg={6} style={{padding: "0"}}>
-            <Image src={withPrefix("/img/deck.jpg")} alt="Deck" fluid />
-          </Col>
-        </Row>
-      </Container>
-      <Container fluid>
-        <Row className="bg-dark">
-          <Col md={12} lg={6} style={{padding: "0"}}>
-            <Image src={withPrefix("/img/boomTruck2.jpg")} alt="Delivery truck" fluid />
-          </Col>
-          <Col md={12} lg={6} className={styles.col}>
-            <div>
-              <Subheader text="WE DELIVER" lightText style={{marginTop: 0}} />
-              <p className={styles.moreInfoP}>
-                Serving the Edge of the Wilderness area - Marcell, Bigfork, Effie, as well as Deer River, Balsom, Talmoon, and Grand Rapids areas.
-              </p>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Subheader text="PRODUCTS" />
-        <Quote />
-        <List />
-      </Container>
-      <div id={styles.disclaimerContainer} ref={this.disclaimer} style={{display: "flex"}}>
-        <div id={styles.disclaimer}>
-          <button id={styles.closeDisclaimer} onClick={this.closeDisclaimer}>&times;</button>
-          <p>Disclaimer: This website is NOT the business website for the company pictured herein.  The images and logos are used with permission for demonstration and educational purposes only.</p>
+        <div id={styles.intro}>
+          <Slideshow />
         </div>
-      </div>
-    </Layout>
+        <Container fluid>
+          <Row className="bg-dark">
+            <Col md={12} lg={6} className={styles.col}>
+              <div>
+                <Subheader text="QUALITY MATERIALS" lightText style={{ marginTop: 0 }} />
+                <p className={styles.moreInfoP}>
+                  We are proud to serve contractors and homeowners as your local source for quality building materials.
+                </p>
+              </div>
+            </Col>
+            <Col md={12} lg={6} style={{ padding: "0" }}>
+              <Image src={withPrefix("/img/deck.jpg")} alt="Deck" fluid />
+            </Col>
+          </Row>
+        </Container>
+        <Container fluid>
+          <Row className="bg-dark">
+            <Col md={12} lg={6} style={{ padding: "0" }}>
+              <Image src={withPrefix("/img/boomTruck2.jpg")} alt="Delivery truck" fluid />
+            </Col>
+            <Col md={12} lg={6} className={styles.col}>
+              <div>
+                <Subheader text="WE DELIVER" lightText style={{ marginTop: 0 }} />
+                <p className={styles.moreInfoP}>
+                  Serving the Edge of the Wilderness area - Marcell, Bigfork, Effie, as well as Deer River, Balsom, Talmoon, and Grand Rapids areas.
+                </p>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
+          <Subheader text="PRODUCTS" />
+          <Quote />
+          <List />
+        </Container>
+        <div id={styles.disclaimerContainer} ref={this.disclaimer} style={{ display: "flex" }}>
+          <div id={styles.disclaimer}>
+            <button id={styles.closeDisclaimer} onClick={this.closeDisclaimer}>&times;</button>
+            <p>Disclaimer: This website is NOT the business website for the company pictured herein.  The images and logos are used with permission for demonstration and educational purposes only.</p>
+          </div>
+        </div>
+      </Layout>
     )
   }
 }
